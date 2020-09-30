e443b2b3b47e729301d4319eff107cdeedae1cbe
Meimaris eShop <meimariscom@gmail.com>
2020-10-01 02:44:59 +0300

    okay done fuck this

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p id="header">Σχολεία Κλειστά Λόγο Του Covid-19</p>
+    <p id="credits">designed by <a href="https://github.com/pasenidis">ed</a> & <a href="https://github.com/TasosY2K" target="_blank">tasos</a></p>
+    <div id="card-container">
+      <div v-for="i in schoolData" :key="i">
+        <Card :info="i"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
+  },
+  data () {
+    return {
+      schoolData: null
+    }
+  },
+  mounted () {
+    axios.get('http://fuck.plasmasearch.cf').then(response => (this.schoolData = response.data.schools))
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Comfortaa', cursive;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0.5rem;
+}
+
+#header {
+  font-size: 2em;
+}
+
+#credits a {
+  text-decoration: none;
+  color: #FF2950;
+}
+
+#credits a:hover {
+  text-decoration: underline;
+}
+
+#card-container {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="card">
+    <div class="centered">
+      <h3>{{ info.name }}</h3>
+    </div>
+    <p>Περιοχή: {{ info.region }}</p>
+    <p>Διεύθηνση: <a class="link" target="_blank" :href="'http://maps.google.com/?q=' + info.address + ', ' + info.region">{{ info.address }}</a></p>
+    <p>Κλειστά μέχρι: {{ info.closed_until }}</p>
+    <p class="underline">Σχόλια</p>
+    <div class="centered">
+      <div class="comments">
+        <p v-if="info.comments">{{ info.comments }}</p>
+        <p v-else>Δεν υπάρχουν σχόλια</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    info: Object
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  padding: 1rem;
+  text-align: left;
+  color: #e9e9e9;
+  background-color: #292929;
+  border-radius: 8px;
+  transition-duration: 0.3s;
+}
+
+.card:hover {
+  transform: translateY(-2px);
+  -webkit-box-shadow: 0px 10px 27px -2px rgba(0,0,0,0.75);
+  -moz-box-shadow: 0px 10px 27px -2px rgba(0,0,0,0.75);
+  box-shadow: 0px 10px 27px -2px rgba(0,0,0,0.75);
+}
+
+.centered {
+  text-align: center;
+}
+
+.centered h3 {
+  margin-top: 0;
+  font-weight: 800;
+  margin-bottom: 1.6rem;
+}
+
+.link {
+  text-decoration: none;
+  color: #FF2950;
+  padding: 2px;
+  border-radius: 8px;
+  transition-duration: 0.3s;
+}
+
+.link:hover {
+  color: #eeeeee;
+  background-color: #FF2950;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.comments {
+  padding: 0.2rem;
+  background-color: #111111;
+  border-radius: 8px;
+}
+</style>
